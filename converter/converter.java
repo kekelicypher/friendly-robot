219048454e552e871a21b9941cbc4d39df0e0abf
Emmanuel Mishiwo <emmachmish@gmail.com>
2025-09-27 12:21:00 -0700

    just adding all the files

---
+++ converter/converter.java
@@ -0,0 +1,58 @@
+package converter;
+
+// package converter;
+// Unit Converter
+// Background:
+// Conversions between units are common in everyday life (e.g., Celsius to Fahrenheit). This
+// project will help you practice methods and basic math.
+// Task:
+// Write a Java program that converts units.
+// Requirements:
+// • - Prompt the user to choose a conversion type (e.g., Celsius ↔ Fahrenheit, kilometers ↔
+// miles).
+// • - Create methods for each conversion.
+// • - Display the converted value.
+// Example Run:
+// Input: 0 Celsius
+// Output: 32 Fahrenheit
+
+import java.util.Scanner;
+
+class converter{
+
+    static double convert(double temperature) {
+        double fahrenheit = (temperature * (9.0/5.0) + 32);
+        return fahrenheit;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Press 1 for 'Celsius ↔ Fahrenheit', and 2 for 'kilometers ↔ miles': ");
+        int choice = scanner.nextInt();
+
+        if (choice.equals(1)) {
+            temp
+        }
+
+
+        System.out.print("Enter temperature in Degrees: ");
+
+
+        double temperature = scanner.nextDouble();
+
+        System.out.println("Fahrenheit: " + convert(temperature));
+
+
+
+
+
+
+
+
+
+
+        scanner.close();
+    }
+}
