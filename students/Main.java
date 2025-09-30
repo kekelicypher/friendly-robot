381829eecb61440acd1b9721d51e7007f94ba36d
Emmanuel Mishiwo <emmachmish@gmail.com>
2025-09-29 19:55:30 -0700

    fixed some bugs

--- students/Main.java
+++ students/Main.java
@@ -2,7 +2,7 @@ public class Main{
 
     public static void main(String[] args) {
 
-        Students student1 = new Students();
+        Student student1 = new Student("Mishiwo", 22, 5);
 
         
     }
